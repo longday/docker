7d60a917f890197c7017c4623c38546902fc0044
longday <6600gm@gmail.com>
2020-08-12 16:42:21 +0300

    alpine version tag

--- netcore/asp-alpine/build_3.1.401.sh
+++ netcore/asp-alpine/build_3.1.401.sh
@@ -4,14 +4,14 @@ trap 'echo "Exit"; exit 1' INT
 ROOT=$(pwd)
 
 VERSION=3.1.401
-
+ALPINE=3.11
 
 cd $VERSION
 ASP_TAG=longday/asp-alpine:${VERSION}
 MAKE_ASP () {
     TYPE=$1
-    BASE_TAG=${ASP_TAG}-${TYPE}
-    NODE_TAG=${ASP_TAG}-node-${TYPE}
+    BASE_TAG=${ASP_TAG}-${ALPINE}-${TYPE}
+    NODE_TAG=${ASP_TAG}-${ALPINE}-node-${TYPE}
     docker build -f Dockerfile.base.${TYPE} --tag ${BASE_TAG}  .
     docker build -f Dockerfile.node.any --build-arg BASE="${BASE_TAG}" --tag ${NODE_TAG} . 
     docker push ${BASE_TAG}
