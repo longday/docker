9ff0432d7b4a00004e026195da62ce42400f43f2
longday <6600gm@gmail.com>
2021-02-11 01:57:20 +0300

    update nginx dev docker

--- nginx/dev/1.19.6/build.sh
+++ nginx/dev/1.19.6/build.sh
@@ -2,9 +2,9 @@
 set -x
 export DOCKER_BUILDKIT=1
 
-VERSION=1.19.6
+VERSION=1.19.6-1
 
-DOCKER_TAG=longday/nginx:${VERSION}-alpine-dev
+DOCKER_TAG=longday/nginx:alpine-dev-${VERSION}
 
 docker build --tag ${DOCKER_TAG} .
 docker push ${DOCKER_TAG}
--- nginx/dev/1.19.6/start.sh
+++ nginx/dev/1.19.6/start.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 
+# while true; do
+#   sleep 2;
+#   /usr/bin/inotifywait -qmr --event modify --format '%e' /etc/nginx | echo ""
+#   echo "nginx config reload"
+#   nginx -s reload
+# done &
+
+folder=/etc/nginx/server.d/*
+md5val=$(md5sum $folder 2>&1 | md5sum)
+
 while true; do
-  sleep 2;
-  /usr/bin/inotifywait -qmr --event modify --format '%e' /etc/nginx | echo ""
+ md5test=$(md5sum $folder 2>&1 | md5sum)
+ if [[ "$md5val" != "$md5test" ]]; then
+  md5val=$md5test
   echo "nginx config reload"
   nginx -s reload
+ fi
+ sleep 2
 done &
 
 nginx -g "daemon off;"
