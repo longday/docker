6265f570b49a3afb7f327ef4af6c438e07684ab9
longday <6600gm@gmail.com>
2021-07-06 14:24:06 +0300

    update nginx

--- nginx/dev/build.sh
+++ nginx/dev/build.sh
@@ -2,10 +2,10 @@
 set -x
 export DOCKER_BUILDKIT=1
 
-VERSION=1.19.6-1
+VERSION=1.21.0
 
 DOCKER_TAG=longday/nginx:alpine-dev-${VERSION}
 
-docker build --tag ${DOCKER_TAG} .
+docker build --tag ${DOCKER_TAG} --build-arg VERSION=${VERSION} .
 docker push ${DOCKER_TAG}
 
--- nginx/react/build.sh
+++ nginx/react/build.sh
@@ -3,13 +3,14 @@ set -x
 trap 'echo "Exit"; exit 1' INT
 ROOT=$(pwd)
 
-VERSION=1.19.6-3
+VERSION=1.21.0
+PATCH=0
 
 SERVER_TAG=longday/nginx:react-server-${VERSION}
 REACT_TAG=longday/nginx:react-app-init-${VERSION}
 PREACT_TAG=longday/nginx:preact-app-init-${VERSION}
 
-docker build --tag ${SERVER_TAG} ./server || exit 1
+docker build --tag ${SERVER_TAG} --build-arg VERSION=${VERSION} ./server || exit 1
 docker build --tag ${REACT_TAG} ./react || exit 1
 docker build --tag ${PREACT_TAG} ./preact || exit 1
 
