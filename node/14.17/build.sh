4c6f7d1f8771be82fbcf978fb69868f05afb8613
longday <6600gm@gmail.com>
2021-07-08 18:18:30 +0300

    update node

--- node/14.17/build.sh
+++ node/14.17/build.sh
@@ -4,7 +4,7 @@ set -x
 export DOCKER_BUILDKIT=1
 
 # https://hub.docker.com/_/node/?tab=description&page=1&ordering=last_updated&name=buster
-VERSION=14.17.1
+VERSION=14.17.3
 NODE_TAG=longday/node:${VERSION}-buster
 
 docker build --tag ${NODE_TAG} --build-arg VERSION=${VERSION} . || exit 1
